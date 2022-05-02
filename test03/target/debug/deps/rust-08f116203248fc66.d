8b7d0ebac68c60bea2a0ff7ec41a668e965fb8f7
aesop <1477903972@qq.com>
2022-05-02 16:15:22 +0800

    删除target目录

--- test03/target/debug/deps/rust-08f116203248fc66.d
+++
@@ -1,5 +0,0 @@
-/Users/k/workspace/rust/test03/target/debug/deps/rust-08f116203248fc66: src/main.rs
-
-/Users/k/workspace/rust/test03/target/debug/deps/rust-08f116203248fc66.d: src/main.rs
-
-src/main.rs:
--- test03/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/Users/k/workspace/rust/test03/target/debug/rust: /Users/k/workspace/rust/test03/src/main.rs
